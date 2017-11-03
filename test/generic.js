8f3a9d7a7a0c69d320103a8207c261b4276a42e7
Mark Hernandez <markedcode@gmail.com>
2017-11-02 23:30:46 -0500

    Improve test suite

---
+++ test/generic.js
@@ -0,0 +1,46 @@
+import path from 'path'
+import assert from 'yeoman-assert'
+import helpers from 'yeoman-test'
+import {files} from './utils/common'
+
+describe('generator-lionbyte:generic', function () {
+  this.timeout(5000)
+
+  describe('scaffolds a generic project with cli', () => {
+    before(() => {
+      return helpers.run(path.join(__dirname, '../src/app'))
+      .withPrompts({
+        name: 'test',
+        description: 'testing generic',
+        version: '0.0.0',
+        projectType: 'generic',
+        cli: true,
+        command: 'heck'
+      })
+    })
+
+    it('creates files', () => {
+      assert.file([
+        'src/cli.js',
+        ...files
+      ])
+    })
+  })
+
+  describe('scaffolds a generic project', () => {
+    before(() => {
+      return helpers.run(path.join(__dirname, '../src/app'))
+      .withPrompts({
+        name: 'test',
+        description: 'testing the generator-lionbyte',
+        'version': '0.0.0',
+        projectType: 'generic',
+        cli: false
+      })
+    })
+
+    it('creates files', () => {
+      assert.file(files)
+    })
+  })
+})
---
+++ test/utils/common.js
@@ -0,0 +1,13 @@
+export const files = [
+  '.babelrc',
+  '.editorconfig',
+  '.gitignore',
+  '.gitattributes',
+  'src/index.js',
+  'test/index.js',
+  'CHANGELOG.md',
+  'LICENSE.md',
+  'README.md',
+  'package.json',
+  'gulpfile.babel.js'
+]
