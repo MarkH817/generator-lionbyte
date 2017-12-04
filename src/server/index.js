6419ab918af492235e74b5eb036f44d4c069c94c
Mark Hernandez <markedcode@gmail.com>
2017-12-04 11:53:28 -0600

    Add starter server files

---
+++ src/server/index.js
@@ -0,0 +1,17 @@
+/** @module */
+import Generator from 'yeoman-generator'
+// import {copy, copyTpl} from '../utils'
+
+module.exports = class Server extends Generator {
+  prompting () {
+
+  }
+
+  writing () {
+
+  }
+
+  install () {
+
+  }
+}
