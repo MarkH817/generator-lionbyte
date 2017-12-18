bdba629752eff40aeffb33510864bb11a84a2650
Mark Hernandez <markedcode@gmail.com>
2017-12-18 13:32:01 -0600

    Update test suite

--- test/static-site.js
+++ test/static-site.js
@@ -7,6 +7,7 @@ describe('generator-lionbyte:static-site', function () {
   this.timeout(timeout)
 
   const projectFiles = [
+    ...files,
     'pages/components/header.ejs',
     'pages/components/metas.ejs',
     'pages/components/stylesheets.ejs',
@@ -23,56 +24,53 @@ describe('generator-lionbyte:static-site', function () {
   describe('scaffolds a static-site', () => {
     before(() => {
       return helpers.run(path.join(__dirname, '../src/app'))
-      .withPrompts({
-        name: 'test',
-        description: 'testing static-site',
-        version: '0.0.0',
-        projectType: 'static-site',
-        react: false
-      })
+        .withPrompts({
+          name: 'test',
+          description: 'testing static-site',
+          version: '0.0.0',
+          projectType: 'static-site',
+          react: false
+        })
     })
 
-    it('creates files', () => {
-      assert.file([
-        ...projectFiles,
-        ...files
-      ])
+    it('creates files', (done) => {
+      assert.file(projectFiles)
+      done()
     })
 
-    it('does not containt React in the configuration files', () => {
+    it('does not containt React in the configuration files', (done) => {
       assert.noFileContent('.babelrc', /react/)
       assert.noFileContent('webpack.common.js', /react/)
+      done()
     })
 
-    it('does not create App.js', () => {
-      assert.noFile([
-        'src/App.js'
-      ])
+    it('does not create App.js', (done) => {
+      assert.noFile(['src/App.js'])
+      done()
     })
   })
 
   describe('scaffolds a static-site with React', () => {
     before(() => {
       return helpers.run(path.join(__dirname, '../src/app'))
-      .withPrompts({
-        name: 'test',
-        description: 'testing static-site',
-        version: '0.0.0',
-        projectType: 'static-site',
-        react: true
-      })
+        .withPrompts({
+          name: 'test',
+          description: 'testing static-site',
+          version: '0.0.0',
+          projectType: 'static-site',
+          react: true
+        })
     })
 
-    it('creates files', () => {
-      assert.file([
-        ...projectFiles,
-        ...files
-      ])
+    it('creates files', (done) => {
+      assert.file([...projectFiles, 'src/App.js'])
+      done()
     })
 
-    it('contains React in the configuration files', () => {
+    it('contains React in the configuration files', (done) => {
       assert.fileContent('.babelrc', /react/)
       assert.fileContent('webpack.common.js', /react/)
+      done()
     })
   })
 })
