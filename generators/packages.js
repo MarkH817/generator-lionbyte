b2827fb6e384fbf901c6b91d3b31d98e562f7448
Mark Hernandez <mark@lion-byte.com>
2020-09-20 12:37:42 -0500

    fix: scope dependency versions to prevent breaking changes
    Previously was installing the latest of every dependency on install step.
    However, later updates added breaking changes, creating setup friction.
    It should now limit certain dependencies to their major version.

--- generators/packages.js
+++ generators/packages.js
@@ -7,59 +7,59 @@
 exports.getPackages = function getPackages (options) {
   /** @type {Array<string>} */
   const devDependencies = [
-    '@lionbyte/eslint-config',
-    '@types/jest',
-    '@types/node',
-    'eslint',
-    'jest',
-    'jest-runner-eslint',
-    'prettier',
-    'typescript'
+    '@lionbyte/eslint-config@latest',
+    '@types/jest@latest',
+    '@types/node@latest',
+    'eslint@latest',
+    'jest@latest',
+    'jest-runner-eslint@latest',
+    'prettier@2',
+    'typescript@latest'
   ]
   /** @type {Array<string>} */
   const dependencies = []
 
   if (options.gitHooks) {
-    devDependencies.push('husky', 'lint-staged')
+    devDependencies.push('husky@4', 'lint-staged@latest')
   }
 
   if (options.projectType === 'frontend') {
     devDependencies.push(
-      '@babel/core',
-      '@babel/preset-env',
-      '@types/webpack',
-      '@types/webpack-bundle-analyzer',
-      '@types/webpack-dev-server',
-      '@types/webpack-merge',
-      'autoprefixer',
-      'babel-jest',
-      'babel-loader',
-      'clean-webpack-plugin',
-      'css-loader',
-      'cssnano',
-      'file-loader',
-      'less',
-      'less-loader',
-      'postcss-loader',
-      'style-loader',
-      'mini-css-extract-plugin',
-      'html-webpack-plugin',
-      'webpack',
-      'webpack-bundle-analyzer',
-      'webpack-cli',
-      'webpack-dev-server',
-      'webpack-merge'
+      '@babel/core@latest',
+      '@babel/preset-env@latest',
+      '@types/webpack@latest',
+      '@types/webpack-bundle-analyzer@latest',
+      '@types/webpack-dev-server@latest',
+      '@types/webpack-merge@latest',
+      'autoprefixer@9',
+      'babel-jest@latest',
+      'babel-loader@latest',
+      'clean-webpack-plugin@3',
+      'css-loader@3',
+      'cssnano@4',
+      'file-loader@6',
+      'less@3',
+      'less-loader@6',
+      'postcss-loader@3',
+      'style-loader@1',
+      'mini-css-extract-plugin@latest',
+      'html-webpack-plugin@4',
+      'webpack@4',
+      'webpack-bundle-analyzer@3',
+      'webpack-cli@3',
+      'webpack-dev-server@3',
+      'webpack-merge@5'
     )
-    dependencies.push('core-js@3', 'regenerator-runtime')
+    dependencies.push('core-js@3', 'regenerator-runtime@latest')
   }
 
   if (options.react) {
     devDependencies.push(
-      '@babel/preset-react',
-      '@types/react',
-      '@types/react-dom'
+      '@babel/preset-react@latest',
+      '@types/react@latest',
+      '@types/react-dom@latest'
     )
-    dependencies.push('react', 'react-dom')
+    dependencies.push('react@latest', 'react-dom@latest')
   }
 
   devDependencies.sort()
